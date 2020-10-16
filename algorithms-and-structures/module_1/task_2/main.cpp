cf12337aefb502b87e2444b0d518d4a9d543379f
Иван <Ivan@iMac-Aleksandr.local>
2020-10-17 02:49:24 +0300

    task_2 tests passed

--- algorithms-and-structures/module_1/task_2/main.cpp
+++ algorithms-and-structures/module_1/task_2/main.cpp
@@ -1,33 +1,11 @@
 #include <iostream>
 
-int main() {
-    // input
-    int n, m;
-    std::cin >> n;
-    int* a_arr = new int[n];
-    for (int i = 0; i < n; i++) std::cin >> a_arr[i];
-
-
-    std::cin >> m;
-    int* b_arr = new int[m];
-    for (int i = 0; i < m; i++) std::cin >> b_arr[i];
-
-    /*
-    for (int i = 0; i < n; i++) std::cout << a_arr[i] << std::endl;
-    std::cout << std::endl;
-    for (int i = 0; i < m; i++) std::cout << b_arr[i] << std::endl;
-    */
-
-    // TODO: реализовать корректную обработку, что между 10 и 20 16 пойдет к индексу 20 (если так вообще надо)
-    // logic
-    int start_index = 0;
-
-    for (int i = 0; i < m; i++) {
+int binary_search_index(const int* a_arr, int element, int start_index, int end_index) {
         int first = start_index;
-        int last = n;
+        int last = end_index;
         while (first < last) {
             int mid = (first + last) / 2;
-            if (a_arr[mid] < b_arr[i]) {
+            if (a_arr[mid] < element) {
                 first = mid + 1;
             }
             else {
@@ -36,26 +14,55 @@ int main() {
         }
 
         // если элементы совпадают
-        if (a_arr[first] == b_arr[i]) {
-            std::cout << b_arr[i] << ": " << first << " " << std::endl;
-            start_index = first + 1;
+        if (a_arr[first] == element) {
+            return first;
         }
         // если элемент больше последнего
-        else if (a_arr[first] < b_arr[i]) {
-            std::cout << b_arr[i] << ": " << first-1 << " " << std::endl;
-            start_index = first - 1;
+        if (a_arr[first] < element) {
+            return first-1;
         }
         // если элемент между двумя
-        else if ((a_arr[first] > b_arr[i]) && (b_arr[i] >= a_arr[0])) {
-            std::cout << b_arr[i] << ": " << first-1 << " " << std::endl;
-            start_index = first;
+        if ((a_arr[first] > element) && (element >= a_arr[0])) {
+            if (std::abs(element - a_arr[first]) < std::abs(element - a_arr[first-1]))
+                return first;
+            else
+                return first - 1;
         }
         // если элемент меньше первого
-        else if (a_arr[first] > b_arr[i]) {
-            std::cout << b_arr[i] << ": " << first << " " << std::endl;
-            start_index = first;
+        if (a_arr[first] > element) {
+            return first;
         }
+        return -1;
+}
+
+int main() {
+    // input
+    int n, m;
+    std::cin >> n;
+    int* a_arr = new int[n];
+    for (int i = 0; i < n; i++) std::cin >> a_arr[i];
+    std::cin >> m;
+    int* b_arr = new int[m];
+    for (int i = 0; i < m; i++) std::cin >> b_arr[i];
+
+    // logic
+    for (int i = 0; i < m; i++) {
+        int element = b_arr[i];
+        int j = 1;
+        while (j < n) {
+            if (element <= a_arr[j]) {
+                int index = binary_search_index(a_arr, element, j / 2, j);
+                std::cout << index << " ";
+                break;
+            }
+            j *= 2;
+        }
+        if (j >= n) std::cout << binary_search_index(a_arr, element, 0, n) << " ";
     }
+
+    // free memory
+    delete[] a_arr;
+    delete[] b_arr;
     return 0;
 }
 
